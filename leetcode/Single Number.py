2f70bf93c5f5997b6427969ef5b42b883ecbe1a5
MOSA PRAVEEN <160846996+mosapraveen@users.noreply.github.com>
2025-08-31 01:05:23 +0530

    Create Single Number.py

---
+++ leetcode/Single Number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        praveen={}
+        for i in nums:
+            praveen[i]=praveen.get(i,0)+1
+        for i in praveen:
+            if(praveen[i]==1):
+                return i
+        
